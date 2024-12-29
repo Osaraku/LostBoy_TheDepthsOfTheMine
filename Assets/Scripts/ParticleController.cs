dbca3e48dba85a5c981054e3eb85528bdd6be486
M Tio Ariyanto <tio.ariyanto313@gmail.com>
2024-12-29 22:41:06 +0700

    menambahkan partikel debu ketika berjalan, terjatuh, dan menyentuh tembok

---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    [SerializeField] ParticleSystem movementParticle;
+    [SerializeField] ParticleSystem fallParticle;
+    [SerializeField] ParticleSystem touchParticle;
+
+    [Range(0, 10)]
+    [SerializeField] int occurAfterVelocity;
+
+    [Range(0, 0.2f)]
+    [SerializeField] float dustFormationPeriod;
+
+    [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] TouchingDirection playerTd;
+    [SerializeField] PlayerController playerController;
+    private bool isFalling;
+    private bool isTouching;
+
+    float counter;
+
+    private void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (playerController.isFacingRight)
+        {
+            movementParticle.transform.localScale = new Vector3(1, 1, 1);
+            fallParticle.transform.localScale = new Vector3(1, 1, 1);
+            touchParticle.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            movementParticle.transform.localScale = new Vector3(-1, 1, 1);
+            fallParticle.transform.localScale = new Vector3(-1, 1, 1);
+            touchParticle.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (!playerTd.isGrounded)
+        {
+            isFalling = true;
+        }
+
+        if (!playerTd.isOnWall)
+        {
+            isTouching = true;
+        }
+
+        if (playerTd.isGrounded && Mathf.Abs(playerRb.velocity.x) > occurAfterVelocity)
+        {
+            if (Mathf.Abs(playerRb.velocity.x) > occurAfterVelocity)
+                if (counter > dustFormationPeriod)
+                {
+                    movementParticle.Play();
+                    counter = 0;
+                }
+        }
+
+        if (playerTd.isGrounded && isFalling)
+        {
+            fallParticle.Play();
+            isFalling = false;
+        }
+
+        if (playerTd.isOnWall && isTouching)
+        {
+            touchParticle.Play();
+            isTouching = false;
+        }
+    }
+
+
+}
